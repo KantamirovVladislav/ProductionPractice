0d5ed9f1a4ffc048d0f72001d9f959bff091a4cf
emil mamedov <mamedovemil366@gmail.com>
2024-06-23 23:35:11 +0300

    update

--- ScheduleWPF/Specialty.cs
+++ ScheduleWPF/Specialty.cs
@@ -8,7 +8,7 @@ public partial class Specialty
 
     public string SpecialtyName { get; set; } = null!;
 
-    public virtual ICollection<Group> Groups { get; set; } = new List<Group>();
+    //public virtual ICollection<Group> Groups { get; set; } = new List<Group>();
 
-    public virtual ICollection<Subject> Subjects { get; set; } = new List<Subject>();
+    //public virtual ICollection<Subject> Subjects { get; set; } = new List<Subject>();
 }
